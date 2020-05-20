1ff42d45c2257ea52f3194e4a3f8c9b02d5c22e7
Arif Hanif <arif.hanif3@gmail.com>
2020-05-20 13:49:02 -0400

    Add project files.

---
+++ Components/Site.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HVACLoad.Components
+{
+    class Site
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Elevation { get; set; }
+    }
+}
---
+++ HVACLoad.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HVACLoad
+{
+    public class HVACLoad
+    {
+    }
+}
